ce5ce86ea5fb116d6acbb22774655b8c8004c6a5
Jamie Jonas <jamie@oneten.tech>
2024-11-26 20:21:00 +0100

    reset the port

--- config/server.ts
+++ config/server.ts
@@ -3,7 +3,7 @@ import { url } from "inspector";
 export default ({ env }) => ({
   host: env('HOST', 'localhost'),
   url: 'oneten-costakids-dev-cms.azurewebsites.net',
-  port: env.int('PORT', 443),
+  port: env.int('PORT', 8080),
   app: {
     keys: env.array('APP_KEYS'),
   },

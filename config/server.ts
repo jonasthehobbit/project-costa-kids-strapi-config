5bc9e8e4e4c93c9fc4fd475a2e183ed4f37ab57e
Jamie Jonas <jamie@oneten.tech>
2024-11-26 18:44:40 +0100

    changed sert ts settings

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,9 @@
+import { url } from "inspector";
+
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', 'localhost'),
+  url: 'oneten-costakids-dev-cms.azurewebsites.net',
+  port: env.int('PORT', 443),
   app: {
     keys: env.array('APP_KEYS'),
   },
